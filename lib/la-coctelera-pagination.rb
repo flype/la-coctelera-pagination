cb4382bc0fee59c559357ee7d1afa9fbc3838ad9
Fernando Blat Peris <ferblape@gmail.com>
2008-12-10 19:46:02 +0100

    some bugs

--- lib/la-coctelera-pagination.rb
+++ lib/la-coctelera-pagination.rb
@@ -15,11 +15,11 @@ module LaCoctelera
       @page = page.nil? ? 1 : page.to_i
       @per_page = per_page.to_i
       @limit = limit
-      @params = params
+      @params = { :action => params['action'] , :controller => params['controller'], :username => params['username']}
       @global_page = (page-1)/(limit/per_page) rescue 0
       @global_page = 0 if @global_page < 0
-      @key_name = Digest::MD5.hexdigest("#{params.to_s}##{@global_page}")
-      @next_key_name = Digest::MD5.hexdigest("#{params.to_s}##{@global_page+1}")
+      @key_name = Digest::MD5.hexdigest("#{@params.to_s}##{extra_identifier}##{@global_page}")
+      @next_key_name = Digest::MD5.hexdigest("#{@params.to_s}##{extra_identifier}##{@global_page+1}")
       
       reference_page = (@page%(@limit/@per_page))
       reference_page = (@limit/@per_page) if reference_page == 0
@@ -28,30 +28,24 @@ module LaCoctelera
       @upper_limit = (reference_page)*@per_page      
     end
     
-    def calculate_total_entries(collection)
-
-      # Si estamos en el último o penúltimo segmento calculamos el segmento siguiente para detectar 
-      # si hay más elementos que paginar o no      
-      next_collection = []
-      if @upper_limit + @per_page >= @limit
-        next_collection = get_or_set_from_cache(@next_key_name, 1.minute) do
-          yield @limit, (@global_page+1)*@limit
-        end
-      end
-      
-      total_entries = next_collection.empty? ? (@global_page*@limit) + collection.size : (@global_page+1)*@limit + next_collection.size
-      total_entries = collection.size + @per_page if total_entries < collection.size
-      total_entries
-    end
-      
     def self.create(page, per_page, limit, params, &block)
       pager = new(page, per_page, limit, params)
 
-      posts = get_or_set_from_cache(pager.key_name, 1.minute) do
+      posts = Pagination.get_or_set_from_cache(pager.key_name, 1.minute) do
         yield pager.limit, pager.global_page*pager.limit
       end
       
-      total_entries = pager.calculate_total_entries(posts)
+      # total_entries = pager.calculate_total_entries(posts)
+      # Si estamos en el último o penúltimo segmento calculamos el segmento siguiente para detectar 
+      # si hay más elementos que paginar o no      
+      next_collection = []
+      if pager.upper_limit + pager.per_page >= pager.limit
+        next_collection = Pagination.get_or_set_from_cache(pager.next_key_name, 1.minute) do
+          yield pager.limit, (pager.global_page+1)*pager.limit
+        end
+      end
+      total_entries = next_collection.empty? ? (pager.global_page*pager.limit) + posts.size : (pager.global_page+1)*pager.limit + next_collection.size
+      total_entries = posts.size + pager.per_page if total_entries < posts.size
       
       WillPaginate::Collection.create(pager.page, pager.per_page, total_entries) do |will_paginate_pager|
         if posts.size >= pager.upper_limit
@@ -64,21 +58,19 @@ module LaCoctelera
       end
     end
     
-    private
-    
-      def self.get_or_set_from_cache(key_name, ttl, &block)
-        unless CACHE.get(key_name)
-          result = yield
-          begin
-            CACHE.set(key_name, result, ttl)
-          rescue MemCache::MemCacheError
-          end
-          result
-        else
-          CACHE.get(key_name)
-        end        
-      end
+    def self.get_or_set_from_cache(key_name, ttl, &block)
+      unless CACHE.get(key_name)
+        result = yield
+        begin
+          CACHE.set(key_name, result, ttl)
+        rescue MemCache::MemCacheError
+        end
+        result
+      else
+        CACHE.get(key_name)
+      end        
+    end
     
   end
-  
+    
 end
